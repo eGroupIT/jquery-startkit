d704e44036d4ca2d49713566e03ce4f892934bfa
SPenguin <egroup.jerrylee@gmail.com>
2018-03-06 11:27:57 +0800

    fixed config pos

--- build/utils/assetsPath.js
+++ build/utils/assetsPath.js
@@ -1,6 +1,6 @@
 'use strict'
 const path = require('path')
 
-const config = require('../../config')
+const config = require('../config')
 
 module.exports = _path => path.posix.join(config.common.assetsSubDirectory, _path)
